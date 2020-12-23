f0e73814cdc3aa6480a9c10fa6cb43e3e701022f
feibiao.ma <feibiao.ma@webeye.com>
2020-12-23 17:35:01 +0800

    epmedia

--- app/src/main/java/com/joe/epmediademo/Activity/MainActivity.java
+++ app/src/main/java/com/joe/epmediademo/Activity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.joe.epmediademo.Activity;
 
+import android.Manifest;
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,22 +15,33 @@ import com.joe.epmediademo.R;
 public class MainActivity extends AppCompatActivity {
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE,}, 1000);
+        }
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		findViewById(R.id.bt_one).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this,EditActivity.class));
-			}
-		});
-		findViewById(R.id.bt_more).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this,MergeActivity.class));
-			}
-		});
-	}
+    private void init() {
+        findViewById(R.id.bt_one).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EditActivity.class));
+            }
+        });
+        findViewById(R.id.bt_more).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MergeActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
 }
