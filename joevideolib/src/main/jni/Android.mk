9065f5d0c20aa58a4efad0dbd3e3d3539fcabb16
feibiao.ma <feibiao.ma@webeye.com>
2020-12-24 15:36:23 +0800

    EpMedia Optimize

--- joevideolib/src/main/jni/Android.mk
+++ joevideolib/src/main/jni/Android.mk
@@ -2,42 +2,42 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE :=  libavutil
-LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libavutil.so
+LOCAL_SRC_FILES := ../jniLibs/$(TARGET_ARCH_ABI)/libavutil.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE :=  libswresample
-LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libswresample.so
+LOCAL_SRC_FILES := ../jniLibs/$(TARGET_ARCH_ABI)/libswresample.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE :=  libswscale
-LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libswscale.so
+LOCAL_SRC_FILES := ../jniLibs/$(TARGET_ARCH_ABI)/libswscale.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libavcodec
-LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libavcodec.so
+LOCAL_SRC_FILES := ../jniLibs/$(TARGET_ARCH_ABI)/libavcodec.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libavformat
-LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libavformat.so
+LOCAL_SRC_FILES := ../jniLibs/$(TARGET_ARCH_ABI)/libavformat.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libavfilter
-LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libavfilter.so
+LOCAL_SRC_FILES := ../jniLibs/$(TARGET_ARCH_ABI)/libavfilter.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE :=  libavdevice
-LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libavdevice.so
+LOCAL_SRC_FILES := ../jniLibs/$(TARGET_ARCH_ABI)/libavdevice.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE :=  libpostproc
-LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libpostproc.so
+LOCAL_SRC_FILES := ../jniLibs/$(TARGET_ARCH_ABI)/libpostproc.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 
